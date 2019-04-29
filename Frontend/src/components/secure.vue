f2799da5ad1d971c3bccfc17332cdcbdc94e7b76
Dima Zapolsky <dima_zapolsky@mail.ru>
2019-04-29 11:42:49 +0300

    Login and register components added to Frontend

---
+++ Frontend/src/components/secure.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+
+<script>
+
+    import uploader from 'vue-simple-uploader';
+    import axios from 'axios';
+
+    export default {
+        components: {},
+        name: 'Secure',
+        props: [],
+        data() {
+            return {}
+        },
+        mounted() {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+</style>
